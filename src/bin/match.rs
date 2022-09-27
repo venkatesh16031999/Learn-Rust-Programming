bda96a9602bd2ce2584cf5fb35aa65550e6355b2
venkatesh16031999 <rvenki666@gmail.com>
2022-09-27 22:09:38 +0530

    Rust basics

---
+++ src/bin/match.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let technology = "blockchain";
+
+    match technology {
+        "blockchain" => println!("I am Blockchain Developer"),
+        "fullstack" => println!("I am Full Stack Developer"),
+        _ => println!("I am a student"),
+    }
+}
---
+++ src/bin/variables.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let st = "Hello";
+    let ch = 'A';
+    let n = 1;
+    let f = 0.1;
+    let b = false;
+
+    println!("String: {:?}, Character: {:?}, Integrer: {:?}, Float: {:?}, Boolean: {:?}", st, ch, n, f, b);
+}
