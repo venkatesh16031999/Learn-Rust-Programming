2809086409b961afe320559b5544e4866918bf14
venkatesh16031999 <rvenki666@gmail.com>
2022-10-23 19:55:32 +0530

    code refactor

--- src/bin/if-let.rs
+++ src/bin/if-let.rs
@@ -2,13 +2,13 @@ fn main() {
     let some_value = Some(11);
 
     if let Some(num) = some_value {
-        println!("{:?}", some_value);
+        println!("{:?}", num);
     }
 
     let none_value: Option<i32> = None;
 
     if let Some(num) = none_value {
-        println!("{:?}", none_value);
+        println!("{:?}", num);
     } else {
         println!("Nothing found");
     }
